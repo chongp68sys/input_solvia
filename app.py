6b21a3bb3f4ec3a066fadbfd514f4674d9e0d38b
chongp68sys <pchong68@gmail.com>
2025-01-13 17:14:23 +0000

    fix error on refresh9

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import streamlit as st
-from auth import authenticate_user
+from auth_bak import authenticate_user
 from solvia_caller import solvia_caller
 from conversations import display_conversations
 
-if authenticate_user():
-    st.sidebar.title("📞 Dialler")
-    agent_id = solvia_caller()  # Get the agent_id from the caller function
+
+st.sidebar.title("📞 Dialler")
+agent_id = solvia_caller()  # Get the agent_id from the caller function
 
     # Pass the agent_id to display_conversations
-    if agent_id:  # Ensure agent_id is not empty
+if agent_id:  # Ensure agent_id is not empty
         display_conversations(agent_id)
-else:
-    st.stop()
